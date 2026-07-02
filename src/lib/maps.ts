31154e9f1d2a39dd606aca256399ef0f70ad7e21
MusclePr <muscle@a-b-c-d.com>
2026-07-02 16:31:26 +0900

    マップを３つ追加

--- src/lib/maps.ts
+++ src/lib/maps.ts
@@ -3,19 +3,21 @@ export const ASA_MAP_NAMES: Record<string, string> = {
   "TheIsland_WP:1460513": "アイランド公式改善版",
   "TheCenter_WP": "センター",
   "ScorchedEarth_WP": "スコーチドアース",
+  "ScorchedEarthRM_WP:1465909": "スコーチドアースリボーン",
   "Ragnarok_WP": "ラグナロク",
   "Aberration_WP": "アベレーション",
   "Extinction_WP": "エクスティンクション",
   "Valguero_WP": "バルゲロ",
+  "LostColony_WP": "ロストコロニー",
   "Astraeos_WP": "アストレオス",
-  // "Genesis1_WP": "ジェネシス1",
+  "Genesis1_WP": "ジェネシス１",
   // "CrystalIsles_WP": "クリスタルアイルズ",
   // "Genesis2_WP": "ジェネシス2",
   // "LostIsland_WP": "ロストアイランド",
   // "Fjordur_WP": "フィヨルド",
   "BobsMissions_WP:1005639": "Club ARK",
   "SurvivalOfTheFittest_TheIsland_WP": "Survival Of The Fittest",
-  "LostColony_WP": "ロストコロニー",
+  "epiphany_WP:1193764": "エピファニー",
 };
 
 export function getMapDisplayName(mapRaw: string): string {
